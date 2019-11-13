c514ef13d0736aced1aafc868597addb175e3c17
Yury Kuppa <x-2@mail.ru>
2019-11-13 19:31:07 +0300

    continuing

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import InstaService from '../services/instaService'
+import User from './User'
 
 
 export default class Posts extends Component {
@@ -18,15 +19,15 @@ export default class Posts extends Component {
     // 3. компонент исчезает со страницы\"умирает" - componentWillUnmount()
     // 4. компонент выдаёт критическую ошибку - componentDidCatch()
 
-    componentDidMount(){       //компонент появился на странице и после этого выполняется метод updatePosts/
+    componentDidMount() {       //компонент появился на странице и после этого выполняется метод updatePosts/
         //Т.е., компонент прогружается, появляется на странице и только после этого мы получаем все посты, then обновляем состояние компонента через .then(this.onPostsLoaded) и выкидываем ошибки, в случае ошибок
         this.updatePosts()
     }
 
-    updatePosts(){      //обновляем посты
+    updatePosts() {      //обновляем посты
         this.InstaService.getAllPosts()     //получаем все посты из джейсона через ИнстаСервис. Получаем промис
-        .then(this.onPostsLoaded)       //передаём в then данные из метода onPostsLoaded. 
-        .catch(this.onError)        //обрабатываем ошибку и запишем в state.error
+            .then(this.onPostsLoaded)       //передаём в then данные из метода onPostsLoaded. 
+            .catch(this.onError)        //обрабатываем ошибку и запишем в state.error
     }
 
     onPostsLoaded = (posts) => {
@@ -34,6 +35,7 @@ export default class Posts extends Component {
             posts: posts,       //записываем постс в постс
             error: false        //если никаких ошибок, то так же остаётся фолс
         });
+        console.log(this.state.posts);
     }
 
     onError = (err) => {
@@ -41,15 +43,42 @@ export default class Posts extends Component {
             error: true
         })
 
-        console.log(err);
+    }
+
+    renderItems(arr) {      //обрабатываем полученные данные из джейсона
+        return arr.map(item => {
+            const { name, altname, photo, src, alt, description, id } = item;
+
+            return (
+                //key в данном случае используется для того, чтобы провести рекалькуляцию в случае обновления постов. Это специальный механизм реакта, который позволяет обновлять и добавлять новые данные, не затрагивая старые
+                <div key={id} className='post'>
+                    <User
+                        src={photo}
+                        alt={altname}
+                        name={name}
+                        min={true} />
+                    <img src={src} alt={alt} />
+                    <div className='post__name'>
+                        {name}
+                    </div>
+                    <div className='post__descr'>
+                        {description}
+                    </div>
+                </div>
+            )
+        })
     }
 
 
     render() {
+        const { error, posts } = this.state   //вытаскиваем текущее состояние стейта
+
+        const items = this.renderItems(posts)      //здесь лежат все посты, которые сгенерировались. potst - все посты, которые в данный момент лежат внутри стейта
+
         return (
             <div className='left'>
 
-
+                {items}     {/* вставляем, рендерим на страницу только те посты, которые мы получили */}
 
 
 
@@ -60,7 +89,7 @@ export default class Posts extends Component {
 
 
                 {/* <Post src='https://cdn.images.express.co.uk/img/dynamic/151/750x445/1138008.jpg' alt='inst' />      {/* src & alt - props */}
-                {/* /* <Post src='https://www.sheffield.ac.uk/polopoly_fs/1.532567!/image/sun-sky865.jpg' alt='inst' /> захардкоденная структура */} 
+                {/* /* <Post src='https://www.sheffield.ac.uk/polopoly_fs/1.532567!/image/sun-sky865.jpg' alt='inst' /> захардкоденная структура */}
             </div>
         )
     }
